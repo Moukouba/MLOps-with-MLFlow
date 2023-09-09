a4bc37b2e82329aa5a6c5dc24659c9a819e01fc3
moukouba <m_moukouba@yahoo.fr>
2023-09-09 03:23:06 +0200

    data transformation completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mlopsProject import logger
 from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-#from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlopsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
@@ -22,6 +22,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
